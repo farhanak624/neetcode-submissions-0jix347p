5b5e511e7637777f5f91fa2cb798bf7c2e99b012
MUHAMMED FARHAN A K <farhanak624@gmail.com>
2026-04-28 22:30:34 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+     let refString=strs[0]
+     let prefix=''
+
+     for (let i=0; i<refString.length; i++){
+     let isMatch=true;
+        for (let j=0; j<strs.length; j++){
+        if(i>=strs[j].length){
+            isMatch=false
+            break
+        }
+            if(refString[i]!==strs[j][i]){
+                isMatch=false
+                break
+            }
+        }
+    if(isMatch){
+        prefix+=refString[i]
+        console.log(prefix)
+    }else{
+        break
+    }
+     }
+     return prefix
+    }
+}
