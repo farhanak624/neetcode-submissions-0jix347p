4d54e73d676ffad9eb4efaef744216804bbbf436
MUHAMMED FARHAN A K <farhanak624@gmail.com>
2026-07-14 23:36:26 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.js
@@ -0,0 +1,38 @@
+class MyHashSet {
+    constructor() {
+        this.data={}
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    add(key) {
+        this.data[key]=1
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        delete this.data[key]
+    }
+
+    /**
+     * @param {number} key
+     * @return {boolean}
+     */
+    contains(key) {
+        const isthere=Object.hasOwn(this.data,key)
+        return isthere
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
